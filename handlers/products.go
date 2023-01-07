ff484450802597efbd29a126715cc0068139b81a
Arnob kumar saha <arnob@appscode.com>
2023-01-07 19:24:49 +0600

    products handler added
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>

---
+++ handlers/products.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Arnobkumarsaha/microservice/data"
+)
+
+type Products struct {
+	lg *log.Logger
+}
+
+func NewProducts(lg *log.Logger) *Products {
+	return &Products{lg: lg}
+}
+
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+
+	// if no method is satisfied return an error
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.lg.Println("Handle GET Products")
+	lp := data.GetProducts()
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"github.com/Arnobkumarsaha/microservice/handlers"
 	"log"
 	"net/http"
@@ -10,19 +11,22 @@ import (
 	"time"
 )
 
+var bindAddr string
+
+func init()  {
+	flag.StringVar(&bindAddr, "BIND_ADDR", ":9090", "usage")
+}
+
 func main() {
 	lg := log.New(os.Stdout, "my-simple-api", log.LstdFlags)
 	mux := http.NewServeMux()
 
-	helloHandler := handlers.NewHello(lg)
-	mux.Handle("/", helloHandler)
-
-	goodbyeHandler := handlers.NewGoodbye(lg)
-	mux.Handle("/bye", goodbyeHandler)
+	productHandler := handlers.NewProducts(lg)
+	mux.Handle("/", productHandler)
 
 
 	server := http.Server{
-		Addr:              ":9090",
+		Addr:              bindAddr,
 		Handler:           mux,
 		ReadTimeout:       time.Second * 10,
 		WriteTimeout:      time.Second * 10,
