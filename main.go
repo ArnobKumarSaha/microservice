8bd55cb16e2c56c6d225ad18badb3416b517049f
Arnob kumar saha <arnob@appscode.com>
2023-01-07 18:39:16 +0600

    Add graceful shutdown
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"github.com/Arnobkumarsaha/microservice/handlers"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -17,7 +20,31 @@ func main() {
 	goodbyeHandler := handlers.NewGoodbye(lg)
 	mux.Handle("/bye", goodbyeHandler)
 
-	http.ListenAndServe(":9090", mux)
+
+	server := http.Server{
+		Addr:              ":9090",
+		Handler:           mux,
+		ReadTimeout:       time.Second * 10,
+		WriteTimeout:      time.Second * 10,
+		IdleTimeout:       time.Minute * 5,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	receivedSignal := <- sigChan
+	lg.Printf("Received signal %s for graceful Shutdown", receivedSignal)
+	timeout, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+	_ = server.Shutdown(timeout)
 }
 
 
